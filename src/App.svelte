5013b879f09964730d660d7f44f4a1bd4cbcffbe
Nixon Kosgei <nkosgey6@gmail.com>
2023-05-18 10:48:08 +0300

    added styles

--- src/App.svelte
+++ src/App.svelte
@@ -3,5 +3,15 @@
 
     let name = "Svelte";
 </script>
-
+<h1>Hello {name.toUpperCase()}!</h1>
 <img src={src} alt="world of {name}">
+
+<p>This is a paragraph.</p>
+
+<style>
+    p {
+        color: goldenrod;
+        font-family: "Comic Sans MS", cursive;
+        font-size: 2em;
+    }
+</style>
